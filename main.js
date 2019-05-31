b60d96d4c1a48489d68f0d253f0d80a48fa15d3d
boegh <henrik@boegh.ninja>
2019-05-31 11:08:10 +0200

    Version 0.1

---
+++ main.js
@@ -0,0 +1,5 @@
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function (tabs) {
+    chrome.windows.create({"url": tabs[0].url, "incognito": true, "state": "maximized"})
+  });
+})
